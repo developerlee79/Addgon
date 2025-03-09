0bcdd0d8d24cbb1e419b9b081030e3eead20bbc4
devle <dltjgus0709@gmail.com>
2025-03-09 22:37:32 +0900

    init: 동아리 박람회 음악 추천 서비스

---
+++ src/main/java/com/devle/adgon/AdgonApplication.java
@@ -0,0 +1,13 @@
+package com.devle.adgon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdgonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdgonApplication.class, args);
+    }
+
+}
